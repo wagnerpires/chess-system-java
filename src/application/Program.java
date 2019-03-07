ed038d5bd7a18617436dbd1f01ebbeb1eac37bd3
Wagner Pires <wagnerpires@hotmail.com>
2019-03-07 06:04:09 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		//Projeto jogo de xadrez em java iniciado em 07/03/2019 - 05:53hs
+		
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 	}
 
 }
