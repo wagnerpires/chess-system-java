f410b02ac58deaa93076ae25921672c0f1cc3540
Wagner Pires <wagnerpires@hotmail.com>
2019-03-10 14:00:46 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,10 +58,16 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK!");
-		}
+			if (!chessMatch.getCheckMate()) {
+				System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+				if(chessMatch.getCheck()) {
+					System.out.println("CHECK!");
+				}
+			}
+			else {
+				System.out.println("CHECKMATE!");
+				System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+			}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) { /** Montagem do tabuleiro na tela */
@@ -75,7 +81,7 @@ public class UI {
 		System.out.println("  a b c d e f g h ");
 	}
 
-	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) { /** * Montagem do tabuleiro na tela mostrando os movimentos possíveis (sobrecarga do método printBoard()) */
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) { /** Montagem do tabuleiro na tela mostrando os movimentos possíveis (sobrecarga do método printBoard()) */
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
