77a9e90653f4e77282c5941dfc4c5c62fcd573a0
Wagner Pires <wagnerpires@hotmail.com>
2019-03-10 19:19:54 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
+	private int moveCount;
 	
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -17,6 +18,18 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount --;
+	}
+	
 //	public void setColor(Color color) {		/** Método retirado para não permitir acesso à alteração de color
 //		this.color = color;
 //	}
